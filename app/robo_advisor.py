dd987c5801c6458aa12b1fd7ff7752d6ca4788ce
sma299 <59669463+sma299@users.noreply.github.com>
2020-02-19 19:31:07 -0500

    get latest day

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -21,13 +21,14 @@ print(dir(response))
 print(response.status_code)
 print(response.text)
 
-#
+# if symbol or API key is wrong
 if "Error Message" in response.text:
     print("OOPS couldn't find that symbol, please try again")
     exit()
 
-print(type(response.text))
 parsed_response = json.loads(response.text)
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
 print(type(parsed_response)) #> dict
 
 
@@ -52,7 +53,7 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY: 2018-02-20")
+print(f"LATEST DAY: {last_refreshed}")
 print("LATEST CLOSE: $100,000.00")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
