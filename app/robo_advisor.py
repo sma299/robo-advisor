0618dcf7f44ddfc6e59125fb4ed171eb6d0676b5
sma299 <59669463+sma299@users.noreply.github.com>
2020-02-22 11:26:45 -0500

    figured out my data validation and started on my datetime function

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -11,8 +11,26 @@ load_dotenv()
 
 API_KEY = os.getenv("ALPHAVANTAGE_API_KEY", default="OOPS")
 
+#for error checking
+has_errors = False
+
 #get the input
 SYMBOL = input("Please input a valid stock ticker: ") #the resulting value is a string
+
+#figure out the length of the input for data validation
+if len(SYMBOL) > 4 or len(SYMBOL) < 1:
+    print("Your stock ticker symbol must be between 1-4 characters!")
+    exit()
+
+#figure out if the variable has integers in it
+try:
+    int_variable = int(SYMBOL)
+    has_errors = True
+except:
+    pass
+if has_errors == True:
+    print("Stock ticker symbol cannot have integers in it!")
+    exit()
 
 
 def to_usd(my_price):
@@ -25,9 +43,8 @@ response = requests.get(request_url)
 
 #if symbol or API key is wrong
 if "Error Message" in response.text:
-    print("OOPS couldn't find that symbol, please try again")
+    print("Unfortunately, that stock ticker symbol does not exist in our database. Please try again.")
     exit()
-
 
 parsed_response = json.loads(response.text)
 
@@ -36,14 +53,14 @@ last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
 
 tsd = parsed_response["Time Series (Daily)"]
 
-# assumes first day is on top but consider sorting
+#assumes first day is on top but consider sorting
 dates = list(tsd.keys())
 
 latest_day = dates[0]
 
 latest_close = parsed_response["Time Series (Daily)"][latest_day]["4. close"]
 
-#get the high price from each day
+#get the high and low price from each day using min and max
 high_prices = []
 low_prices = []
 
@@ -53,15 +70,11 @@ for date in dates:
     low_price = tsd[date]["3. low"]
     low_prices.append(float(low_price))
 
-
 #maximum of all of the high prices
 recent_high = max(high_prices)
 recent_low = min(low_prices)
 
-
-
-#csv_file_path = "prices.csv" # a relative filepath
-
+#best way to get a CSV file path
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
 
 csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
@@ -70,8 +83,7 @@ with open(csv_file_path, "w") as csv_file: # "w" means open the file for writing
     writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
     writer.writeheader()
 
-    #create a loop and write one for each day
-
+    #create a loop and write an entry for each day
     for date in dates:
         daily_prices = tsd[date]
         writer.writerow({
@@ -85,19 +97,25 @@ with open(csv_file_path, "w") as csv_file: # "w" means open the file for writing
 
 
 # info output
-
-
 print("-------------------------")
 print("SELECTED SYMBOL: " + SYMBOL)
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
-print("REQUEST AT: 2018-02-20 02:00pm")
+#datetime function
+today = datetime.datetime.today()
+print("REQUEST AT: " + today.strftime("%Y-%m-%d %I:%M %p"))
+#continue to print the information from the parsed response
 print("-------------------------")
 print(f"LATEST DAY: {last_refreshed}")
 print(f"LATEST CLOSE: {to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
+
+#You are free to develop your own custom recommendation algorithm. This is perhaps one of the most fun and creative parts of this project. 
+#One simple example algorithm would be (in pseudocode):
+#If the stock's latest closing price is less than 20% above its recent low, "Buy", else "Don't Buy".
+
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
 print("-------------------------")
